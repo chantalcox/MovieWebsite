11945e1c3dfb3068bd161129271e0d575ae399c8
Chantal Marin <chantal.marin@berkeley.edu>
2016-07-03 16:20:21 -0700

    image changed

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,7 +4,7 @@ import fresh_tomatoes
 toy_story = media.Movie("Toy story", "A story of a boy and his toys", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=TZeG23jEfHM")
 #print(toy_story.storyline)
 
-avatar = media.Movie("Avatar", "A marine on an alien planet", "https://upload.wikimedia.org/wikipedia/en/3/38/Avatarjakeneytiri.jpg", "https://www.youtube.com/watch?v=cX0R3mXaod8")
+avatar = media.Movie("Avatar", "A marine on an alien planet", "http://media.hollywood.com/images/679x1000/7037214.jpg", "https://www.youtube.com/watch?v=cX0R3mXaod8")
 #print(avatar.storyline)
 
 dory = media.Movie("Finding dory", "A fish that is more than a fish", "http://cinemalive.in/wp-content/uploads/2016/03/finding-dory.png","https://www.youtube.com/watch?v=3JNLwlcPBPI")
