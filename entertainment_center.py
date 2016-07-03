cea18533b7e73b798fca4923e464bfb754ebaa96
Chantal Marin <chantal.marin@berkeley.edu>
2016-07-03 15:12:43 -0700

    general

---
+++ entertainment_center.py
@@ -0,0 +1,6 @@
+import media
+toy_story = media.Movie("Toy story", "A story of a boy and his toys", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=TZeG23jEfHM")
+print(toy_story.storyline)
+
+avatar = media.Movie("Avatar", "A marine on an alien planet", "https://upload.wikimedia.org/wikipedia/en/3/38/Avatarjakeneytiri.jpg", "https://www.youtube.com/watch?v=cX0R3mXaod8")
+print(avatar.storyline)
---
+++ media.py
@@ -0,0 +1,7 @@
+class Movie():
+
+	def __init__ (udacity, movie_title, movie_storyline, poster_image, trailer_youtube):
+		udacity.title = movie_title
+		udacity.storyline= movie_storyline
+		udacity.poster_image_url = poster_image
+		udacity.trailer_youtube_url = trailer_youtube
